d32f2ddec747418d1133efc7b32dc7bd18b77ecb
Bryan Woods <pair+bryan@howaboutwe.com>
2013-01-03 14:20:34 -0500

    More cleaning up

--- enforcer.rb
+++ enforcer.rb
@@ -2,25 +2,33 @@ class Enforcer
   class NoRSpecExampleFoundError < StandardError; end
 
   def self.establish_built_in_classes
-    @built_in_classes ||= ObjectSpace.each_object.select do |object|
-      object.is_a?(Class)
-    end
+    @built_in_classes ||= all_classes
   end
 
   def self.discover_application_classes
-    @application_classes = ObjectSpace.each_object.select do |object|
-      object.is_a?(Class)
-    end.reject { |object| @built_in_classes.include?(object) }
+    @application_classes = all_classes.reject do |klass|
+      @built_in_classes.include?(klass)
+    end
   end
 
   def self.run
     @application_classes.each do |klass|
-      unless RSpec.world.example_groups.map(&:described_class).include?(klass)
+      unless described_classes.include?(klass)
         raise NoRSpecExampleFoundError.new(
           "No spec for `#{klass}` found. Please write a test before proceeding."
         )
       end
     end
+  end
+
+  private
+
+  def self.described_classes
+    RSpec.world.example_groups.map(&:described_class)
+  end
+
+  def self.all_classes
+    ObjectSpace.each_object(Class).to_a
   end
 end
 
